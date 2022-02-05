752275434c3523b1f80c043a8051365bcbfb9f96
Utkarsh <ignition3951@gmail.com>
2022-02-05 20:52:24 +0530

    for each internally uses Consumer Functional interface

--- src/com/features/lambda/ConsumerDemo.java
+++ src/com/features/lambda/ConsumerDemo.java
@@ -1,13 +1,19 @@
 package com.features.lambda;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
 
 	public static void main(String[] args) {
 		
-		Consumer<Integer> consumer = t -> System.out.println("The number is : " + t);
-		consumer.accept(10);
+//		Consumer<Integer> consumer = t -> System.out.println("The number is : " + t);
+//		consumer.accept(10);
+		
+		//for each internally uses Consumer functional interface  as described above
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+		list.stream().forEach(t -> System.out.println("The number is : " + t));
 	}
 
 }
